c42909414ed22d4b0fe27a6e2dd5987bd397c483
Gustavo <gustavo@kapibara.com.br>
2025-09-24 21:19:12 -0300

    Criando multiplas velhas e bloqueando tabuleiros

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,32 @@ import './App.css'
 import Velha from './Velha'
 
 function App() {
+  const [jogadorDaVez, setJogadorDaVez] = useState('X')
+  const [tabuleiroDaVez, setTabuleiroDaVez] = useState(4)
+  const [tabuleiros, setTabuleiros] = useState([
+    '', '', '',
+    '', '', '',
+    '', '', ''
+  ])
 
   return (
     <>
       <h1>Multi Velha</h1>
-      <Velha />
+      <div className='card'>
+        {
+          tabuleiros.map((jogada, index) => {
+            return (
+              <Velha
+                key={index.toString()}
+                jogadorDaVez={jogadorDaVez}
+                setJogadorDaVez={setJogadorDaVez}
+                setTabuleiroDaVez={setTabuleiroDaVez}
+                overlay={tabuleiroDaVez != index}
+              />
+            )
+          })
+        }
+      </div>
     </>
   )
 }
--- src/Velha/index.jsx
+++ src/Velha/index.jsx
@@ -1,84 +1,94 @@
 import { useEffect, useState } from 'react'
 import './index.css'
 
-export default () => {
-  const [jogadorDaVez, setJogadorDaVez] = useState('X')
-  const [ganhador, setGanhador] = useState([]);
-  const [gameover, setGameover] = useState(false);
-  const [jogadas, setJogadas] = useState([
-    '', '', '',
-    '', '', '',
-    '', '', ''
-  ])
+const Overlay = ({ children }) => {
+    return (
+        <div className='block'>{children}</div>
+    )
+}
 
-  useEffect(() => {
-    verificaJogadas()
-  }, [jogadas])
+export default ({ jogadorDaVez, setJogadorDaVez, setTabuleiroDaVez, overlay }) => {
+    const [ganhador, setGanhador] = useState([]);
+    const [gameover, setGameover] = useState(false);
+    const [jogadas, setJogadas] = useState([
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ])
 
-  const verificaJogadas = () => {
-    if (
-      verificaGanhador(0, 1, 2) || //linha1
-      verificaGanhador(3, 4, 5) || //linha2
-      verificaGanhador(6, 7, 8) || //linha3
+    useEffect(() => {
+        verificaJogadas()
+    }, [jogadas])
 
-      verificaGanhador(0, 3, 6) || //coluna1
-      verificaGanhador(1, 4, 7) || //coluna2
-      verificaGanhador(2, 5, 8) || //coluna3
+    const verificaJogadas = () => {
+        if (
+            verificaGanhador(0, 1, 2) || //linha1
+            verificaGanhador(3, 4, 5) || //linha2
+            verificaGanhador(6, 7, 8) || //linha3
 
-      verificaGanhador(0, 4, 8) || //diagonal1
-      verificaGanhador(2, 4, 6)    //diagonal2
-    ) {
-      setGameover(true);
-    }
+            verificaGanhador(0, 3, 6) || //coluna1
+            verificaGanhador(1, 4, 7) || //coluna2
+            verificaGanhador(2, 5, 8) || //coluna3
 
-    const totalDeJogadas = jogadas.filter(x => x != '').length;
-    if (totalDeJogadas == 9)  //Deu velha !!`
-      setGameover(true);
-  }
-
-  const verificaGanhador = (i1, i2, i3) => {
-    const ganhou = (
-      jogadas[i1] != '' && jogadas[i2] != '' && jogadas[i3] != '' &&
-      jogadas[i1] == jogadas[i2] && jogadas[i1] == jogadas[i3]
-    );
-
-    if (ganhou)
-      setGanhador([i1, i2, i3]);
-
-    return ganhou
-  }
-
-  const fazJogada = (posicao) => {
-    if (!gameover) {
-      jogadas[posicao] = jogadorDaVez
-      setJogadas([...jogadas])
-      setJogadorDaVez(jogadorDaVez == 'X' ? 'O' : 'X')
-    }
-  }
-
-  return (
-    <>
-      <h1>Multi Velha</h1>
-      <div className="card">
-        {
-          jogadas.map((jogada, index) => {
-            let classes = "botao "
-            if (jogada == '')
-              classes += "empty "
-            else if (jogada == 'X')
-              classes += "playerX "
-            else if (jogada == 'O')
-              classes += "playerO "
-            if (gameover && ganhador.includes(index))
-              classes += 'win'
-            return (
-              <button key={index.toString()} className={classes} onClick={() => fazJogada(index)}>
-                {jogada}
-              </button>
-            )
-          })
+            verificaGanhador(0, 4, 8) || //diagonal1
+            verificaGanhador(2, 4, 6)    //diagonal2
+        ) {
+            setGameover(true);
         }
-      </div>
-    </>
-  )
+
+        const totalDeJogadas = jogadas.filter(x => x != '').length;
+        if (totalDeJogadas == 9)  //Deu velha !!`
+            setGameover(true);
+    }
+
+    const verificaGanhador = (i1, i2, i3) => {
+        const ganhou = (
+            jogadas[i1] != '' && jogadas[i2] != '' && jogadas[i3] != '' &&
+            jogadas[i1] == jogadas[i2] && jogadas[i1] == jogadas[i3]
+        );
+
+        if (ganhou)
+            setGanhador([i1, i2, i3]);
+
+        return ganhou
+    }
+
+    const fazJogada = (posicao) => {
+        if (!gameover && !overlay) {
+            jogadas[posicao] = jogadorDaVez
+            setJogadas([...jogadas])
+            setTabuleiroDaVez(posicao)
+            setJogadorDaVez(jogadorDaVez == 'X' ? 'O' : 'X')
+        }
+    }
+
+    return (
+        <>
+            <div className="card">
+                {
+                    jogadas.map((jogada, index) => {
+                        let classes = "botao "
+                        if (jogada == '')
+                            classes += "empty "
+                        else if (jogada == 'X')
+                            classes += "playerX "
+                        else if (jogada == 'O')
+                            classes += "playerO "
+                        if (gameover && ganhador.includes(index))
+                            classes += 'win'
+                        return (
+                            <button key={index.toString()} className={classes} onClick={() => fazJogada(index)}>
+                                {jogada}
+                            </button>
+                        )
+                    })
+                }
+                {
+                    overlay && <div className='block'>
+                        <label>{gameover ?? jogadas[ganhador[0]]}</label>
+                    </div>
+                }
+            </div>
+        </>
+    )
 }
